040e52a22cc5c59fb198d3c704a7e0c57991b412
HongHuy2k4 <137086314+HongHuy2k4@users.noreply.github.com>
2023-11-18 09:48:04 +0700

    up

---
+++ cart-product/cartProduct.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="./cartProduct.css">
+  <title>Document</title>
+</head>
+<body>
+  <section class="cart-page">
+    <table>
+      <tr>
+        <th>Sản phẩm trong giỏ hàng <span>3</span></th>
+        <th>Số lượng</th>
+        <th>Giá mua</th>
+        <th>Giảm giá</th>
+        <th>Thành tiền</th>
+      </tr>
+      <tr>
+        <td>
+          <div class="cart-info">
+            <img src="https://kingkongsportwear.vn/wp-content/uploads/2023/01/z4067747139840_dbb0b60b7188ae6968c87962641b0bbb-300x300.jpg" alt="">
+            <div>
+              <p>Áo thun đỏ in họa tiết</p>
+              <a href="">Xóa</a>
+            </div>
+          </div>
+        </td>
+        <td><input type="text" value="1"></td>
+        <td>130.000 đ</td>
+        <td>0 đ</td>
+        <td>130.000 đ</td>
+      </tr>
+
+      <tr>
+        <td>
+          <div class="cart-info">
+            <img src="https://kingkongsportwear.vn/wp-content/uploads/2023/01/z4067747139840_dbb0b60b7188ae6968c87962641b0bbb-300x300.jpg" alt="">
+            <div>
+              <p>Áo thun đỏ in họa tiết</p>
+              <a href="">Xóa</a>
+            </div>
+          </div>
+        </td>
+        <td><input type="text" value="1"></td>
+        <td>130.000 đ</td>
+        <td>0 đ</td>
+        <td>130.000 đ</td>
+      </tr>
+
+      <tr>
+        <td>
+          <div class="cart-info">
+            <img src="https://kingkongsportwear.vn/wp-content/uploads/2023/01/z4067747139840_dbb0b60b7188ae6968c87962641b0bbb-300x300.jpg" alt="">
+            <div>
+              <p>Áo thun đỏ in họa tiết</p>
+              <a href="">Xóa</a>
+            </div>
+          </div>
+        </td>
+        <td><input type="text" value="1"></td>
+        <td>130.000 đ</td>
+        <td>0 đ</td>
+        <td>130.000 đ</td>
+      </tr>
+    </table>
+
+    <div class="total-price">
+        <table>
+          <tr>
+            <td>Tổng tiền hàng</td>
+            <td>390.000 đ</td>
+          </tr>
+
+          <tr>
+            <td>Phí vận chuyển</td>
+            <td>0 đ</td>
+          </tr>
+
+          <tr>
+            <td>Tổng thanh toán</td>
+            <td>390.000 đ</td>
+          </tr>
+        </table>
+      </div>
+  </section>
+</body>
+</html>
