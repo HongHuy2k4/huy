0ae6681061f8a02884c55cb2843a3a63b6b38c80
HongHuy2k4 <137086314+HongHuy2k4@users.noreply.github.com>
2023-11-21 18:55:55 +0700

    up

---
+++ payment gateway/payment gateway/payment.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <form action="">
+            <div class="row">
+                <div class="col">
+                    <h3 class="title">địa chỉ đơn hàng</h3>
+                    <div class="inputBox">
+                        <span>họ tên :</span>
+                        <input type="text" placeholder="Nguyen Van A">
+                    </div>
+                    <div class="inputBox">
+                        <span>email :</span>
+                        <input type="email" placeholder="nva123@gmail.com">
+                    </div>
+                    <div class="inputBox">
+                        <span>địa chỉ :</span>
+                        <input type="text" placeholder="123 - Truong Trinh - Hoa Phat">
+                    </div>
+                    <div class="inputBox">
+                        <span>thành phố :</span>
+                        <input type="text" placeholder="Đà Nẵng">
+                    </div>
+
+                    <div class="flex">
+                        <div class="inputBox">
+                            <span>quốc tịch :</span>
+                            <input type="text" placeholder="Vietnam">
+                        </div>
+                        <div class="inputBox">
+                            <span>mã bưu điện :</span>
+                            <input type="text" placeholder="123 456">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <h3 class="title">thanh toán</h3>
+                    <div class="inputBox">
+                        <span>thẻ cho phép :</span>
+                        <img src="images/card_img.png" alt="">
+                    </div>
+                    <div class="inputBox">
+                        <span>tên trên thẻ :</span>
+                        <input type="text" placeholder="Nguyen Van A">
+                    </div>
+                    <div class="inputBox">
+                        <span>mã số thẻ :</span>
+                        <input type="text" placeholder="1111-2222-3333-4444">
+                    </div>
+                    <div class="inputBox">
+                        <span>tháng hết hạn :</span>
+                        <input type="text" placeholder="Tháng 1">
+                    </div>
+
+                    <div class="flex">
+                        <div class="inputBox">
+                            <span>năm hết hạn :</span>
+                            <input type="number" placeholder="2023">
+                        </div>
+                        <div class="inputBox">
+                            <span>CVV :</span>
+                            <input type="text" placeholder="1234">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <input type="submit" value="Tiến hành kiểm tra" class="submit-btn">
+        </form>
+    </div>
+</body>
+
+</html>
