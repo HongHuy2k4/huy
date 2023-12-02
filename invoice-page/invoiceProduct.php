cfc3750b714178c08964ffd6d21fe1d65f322bac
HongHuy2k4 <137086314+HongHuy2k4@users.noreply.github.com>
2023-12-02 21:09:03 +0700

    up

---
+++ invoice-page/invoiceProduct.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="wrapper">
+    <ul class="navbar">
+      <li><a href="">Tất cả</a></li>
+      <li><a href="">Chờ thanh toán</a></li>
+      <li><a href="">Đang vận chuyển</a></li>
+      <li><a href="">Đã giao</a></li>
+      <li><a href="">Đã hủy</a></li>
+    </ul>
+    <h3>Danh sách hóa đơn</h3>
+    <table>
+      <thead>
+        <tr>
+          <td>STT</td>
+          <td>Ảnh</td>
+          <td>Trạng thái</td>
+          <td>Mô tả sản phẩm</td>
+          <td>Số lượng</td>
+          <td>Giá</td>
+          <td>Xóa</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td><img src="https://tse4.mm.bing.net/th?id=OIP.7ZxepcJaDNoUZqs3JZPxKwHaHa&pid=Api&P=0&h=180" alt=""></td>
+          <td>Chờ thanh toán</td>
+          <td>Lorem ipsum dolor sit</td>
+          <td>5</td>
+          <td>500.000đ</td>
+          <td><a href="">Hủy</i></a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td><img src="https://tse4.mm.bing.net/th?id=OIP.7ZxepcJaDNoUZqs3JZPxKwHaHa&pid=Api&P=0&h=180" alt=""></td>
+          <td>Đang vận chuyển</td>
+          <td>Lorem ipsum dolor sit</td>
+          <td>5</td>
+          <td>500.000đ</td>
+          <td><a href="">Hủy</a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td><img src="https://tse4.mm.bing.net/th?id=OIP.7ZxepcJaDNoUZqs3JZPxKwHaHa&pid=Api&P=0&h=180" alt=""></td>
+          <td>Đã giao</td>
+          <td>Lorem ipsum dolor sit</td>
+          <td>5</td>
+          <td>500.000đ</td>
+          <td><a href="">Hủy</a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td><img src="https://tse4.mm.bing.net/th?id=OIP.7ZxepcJaDNoUZqs3JZPxKwHaHa&pid=Api&P=0&h=180" alt=""></td>
+          <td>Đã hủy</td>
+          <td>Lorem ipsum dolor sit</td>
+          <td>5</td>
+          <td>500.000đ</td>
+          <td><a href="">Hủy</a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td><img src="https://tse4.mm.bing.net/th?id=OIP.7ZxepcJaDNoUZqs3JZPxKwHaHa&pid=Api&P=0&h=180" alt=""></td>
+          <td>Đã thanh toán</td>
+          <td>Lorem ipsum dolor sit</td>
+          <td>5</td>
+          <td>500.000đ</td>
+          <td><a href="">Hủy</a></td>
+        </tr>
+      </tbody>
+    </table>
+    <button class="back-btn"><i class="fa-solid fa-caret-left"></i> Tiếp tục mua</button>
+  </div>
+</body>
+
+</html>
