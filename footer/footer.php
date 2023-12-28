6fbb587d863d8395d431e40dcabfe7a5da07b069
HongHuy2k4 <dhuy24309@gmail.com>
2023-12-28 19:18:19 +0700

    change

---
+++ footer/footer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title>Document</title>
+</head>
+
+<body>
+  <footer>
+    <div class="container">
+      <div class="sec aboutus">
+        <h2>Giới thiệu</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi facere quia inventore ipsam facilis tempora
+          impedit sapiente eligendi sint quod itaque.</p>
+        <ul class="sci">
+          <li><a href="#"><i class="fa-brands fa-facebook-f"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+          <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+        </ul>
+      </div>
+      <div class="sec quicklinks">
+        <h2>Hỗ trợ</h2>
+        <ul>
+          <li><a href="#">Câu hỏi thường gặp</a></li>
+          <li><a href="#">Chính sách bảo mật</a></li>
+          <li><a href="#">Trợ giúp</a></li>
+          <li><a href="#">Liên lạc</a></li>
+        </ul>
+      </div>
+      <div class="sec quicklinks">
+        <h2>Shop</h2>
+        <ul>
+          <li><a href="#">Điện thoại</a></li>
+          <li><a href="#">Laptop</a></li>
+          <li><a href="#">Máy tính bảng</a></li>
+          <li><a href="#">Sản phẩm khác</a></li>
+        </ul>
+      </div>
+      <div class="sec contact">
+        <h2>Liên hệ chúng tôi</h2>
+        <ul class="info">
+          <li>
+            <span><i class="fa-solid fa-phone"></i></span>
+            <p><a href="tel:0123456789">0123456789</a></p>
+          </li>
+          <li>
+            <span><i class="fa-solid fa-envelope"></i></span>
+            <p><a href="mailto:huy123@gmail.com">huy123@gmail.com</a></p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</body>
+
+</html>
